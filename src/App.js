1d324239cac75e7d77c1fe3ecea4fadc540a52c6
huumai1998 <huumai0918@gmail.com>
2023-01-09 19:51:41 -0700

    done-main-page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Nav } from "./components";
+import { Main } from "./pages";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/main.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+export const Main = () => {
+  return (
+    <div className="m-container">
+      <div className="m-container box">
+        <div className="m-name">
+          <h1>
+            Hya, <br /> welcome to my page!
+          </h1>
+        </div>
+
+        <div className="text-title">
+          <p>
+            I am Full-Stack developer with entry-level of experience in web
+            designing and development.
+            <Typewriter
+              cursor
+              typeSpeed={0}
+              cursorStyle="_"
+              words={[""]}
+            ></Typewriter>
+          </p>
+        </div>
+        <div className="i-social-media">
+          <a href="https://github.com/huumai1998" target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/huu-mai-283b1216b/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
