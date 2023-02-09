c9b13fee0079a4a8fb6c1744e5698c585ce950cd
huumai1998 <huumai0918@gmail.com>
2023-02-08 21:59:16 -0700

    yo

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { html } from "../assets";
 
 export const Skills = () => {
   return (
@@ -7,6 +8,10 @@ export const Skills = () => {
         <div className="container">
           <div className="title">
             <h1>SKILLS</h1>
+          </div>
+
+          <div className="">
+            <img src={html} alt="html" width="150px" height="150px" />
           </div>
         </div>
       </div>
