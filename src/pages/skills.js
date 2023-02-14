0ff52f9047d1e8883160273a8f64bda6bf3eb5dc
huumai1998 <huumai0918@gmail.com>
2023-02-13 20:21:25 -0700

    dont skills

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -5,41 +5,48 @@ import { css, html, java, mongoo, node, react, ruby } from "../assets";
 export const Skills = () => {
   return (
     <>
-      <section className="skills" id="skills">
+      <section className="skills" id="skills" data-aos="zoom-in">
         <div className="container">
           <div className="title">
             <h1>SKILLS</h1>
           </div>
 
-          <div className="skills-container">
+          <div className="skills-container" data-aos="zoom-in-up">
             <div className="skills-box">
               <img src={html} alt="html" />
+              <h3>HTML</h3>
             </div>
 
             <div className="skills-box">
               <img src={css} alt="css" />
+              <h3>SCSS/CSS</h3>
             </div>
 
             <div className="skills-box">
               <img src={ruby} alt="ruby" />
+              <h3>Ruby</h3>
             </div>
 
             <div className="skills-box">
               <img src={react} alt="react" />
+              <h3>ReactJS</h3>
             </div>
           </div>
 
-          <div className="skills-container">
+          <div className="skills-container" data-aos="zoom-in-up">
             <div className="skills-box">
               <img src={node} alt="node" />
+              <h3>Rest-API</h3>
             </div>
 
             <div className="skills-box">
               <img src={mongoo} alt="mongoo" />
+              <h3>Mongoose</h3>
             </div>
 
             <div className="skills-box">
               <img src={java} alt="java" />
+              <h3>Java</h3>
             </div>
           </div>
         </div>
